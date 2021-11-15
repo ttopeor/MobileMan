518a2aaab741ac843d1a8581f424b4e1cfd3f7e2
ttopeor <ttopeor@udel.edu>
2021-11-15 15:54:41 +0800

    settimeout

--- server/rtsp_to_ws.js
+++ server/rtsp_to_ws.js
@@ -24,6 +24,7 @@ const rtspServer = new Stream({
   wsPort: 9999,
   ffmpegOptions: {
     // options ffmpeg flags
+    "-stimeout": "1000000000",
     "-stats": "", // an option with no neccessary value uses a blank string
     "-r": 30 // options with required values specify the value after the key
   }
