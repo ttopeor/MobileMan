0c0e39b5d01fcbf2f55b222b4b2923fcbb23ca86
Ubuntu <ubuntu@ip-172-31-24-192.ap-southeast-1.compute.internal>
2021-11-19 09:42:02 +0000

    rework file structure and add restart logic

--- server/rtsp_to_ws.js
+++ server/rtsp_to_ws.js
@@ -3,7 +3,7 @@
  * Socket io
  */
 
- const path = require('path')
+const path = require('path')
 //import express from "express";
 //import http from "http";
 //@ts-ignore
@@ -12,18 +12,17 @@ const express = require('express');
 const http = require('http');
 const Stream = require('node-rtsp-stream')
 
-const app_stream_converter = express();
-const server = http.createServer(app_stream_converter);
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 8080;
 
 
-function start_server(){
+function start_server() {
   console.log("start rtsp server");
   const rtspServer = new Stream({
     name: "camera",
     //streamUrl: "rtsp://192.168.10.40:8554/live",
-    streamUrl: "rtsp://localhost:8554/live",
+    streamUrl: "rtsp://54.254.23.229:8554/live",
     wsPort: 9999,
     ffmpegOptions: {
       // options ffmpeg flags
@@ -31,69 +30,51 @@ function start_server(){
       "-r": 30 // options with required values specify the value after the key
     }
   });
-  rtspServer.on("exitWithError", () => {
-    console.log("server exit");
+
+
+
+  let lastTick = Date.now()
+  let receivedFirstData = false
+  rtspServer.on('camdata', function (data) {
+    receivedFirstData = true
+    lastTick = Date.now()
+  });
+
+  const heartBeat = setInterval(() => {
+    if (Date.now() - lastTick > 5000 && receivedFirstData) {
+      restart()
+    }
+  }, 2000);
+
+
+  const restart = () => {
+    console.log("Restarting server...");
+    rtspServer.stop();
+    clearInterval(heartBeat);
+    start_server();
+  }
+
+  rtspServer.on('exitWithError', () => {
     setTimeout(() => {
-      console.log("reconnecting");
-      start_server();
-    }, 3000);
+      restart();
+    }, 3000)
   })
+
+
 }
 start_server();
+
+
 /**
  * HTTP service
  */
-app_stream_converter.use(express.static("public"));
-
-app_stream_converter.post("/connection", (req, res) => {
-  res.json({ id: 1, state: "good" });
-});
-
+app.use(express.static("/home/ubuntu/MobilemanUI/server/ui"));
 
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/index.html'));
-});
-app.get('/app.js', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/app.js'));
-});
-app.get('/app.css', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/app.css'));
-});
-app.get('/jsmpeg.min.js', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/jsmpeg.min.js'));
-});
-
-app.get('/js/jquery-3.5.1.min.js', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/js/jquery-3.5.1.min.js'));
-});
-app.get('/js/roslib.js', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/js/roslib.js'));
-});
-app.get('/img/camera.png', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/img/camera.png'));
-});
-app.get('/img/compass.png', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/img/compass.png'));
-});
-app.get('/img/map_selector.png', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/img/map_selector.png'));
-});
-app.get('/img/minus.png', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/img/minus.png'));
-});
-app.get('/img/plus.png', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/img/plus.png'));
-});
-app.get('/img/tool.png', function(req, res) {
-  res.sendFile(path.join(__dirname, '../ui/img/tool.png'));
-});
-
-app.listen(port);
 console.log('Server started at http://54.254.23.229:' + port);
 /**
  * Listen
  */
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("Server start running...");
 });
